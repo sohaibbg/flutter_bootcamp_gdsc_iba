54b3e3ea2df3e64744ebecc8157c136457c0139f
Sohaib Baig <sohaibbg@gmail.com>
2023-03-18 07:39:11 +0500

    sabotaged build to test pipeline

--- lib/classes/game.dart
+++ lib/classes/game.dart
@@ -14,17 +14,17 @@ class Game {
 
   static bool hasWon(bool b) {
     const List<List<int>> winningCombos = [
-      // horizontal
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      // vertical
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      // diagonal
-      [0, 4, 8],
-      [2, 4, 6],
+      // // horizontal
+      // [0, 1, 2],
+      // [3, 4, 5],
+      // [6, 7, 8],
+      // // vertical
+      // [0, 3, 6],
+      // [1, 4, 7],
+      // [2, 5, 8],
+      // // diagonal
+      // [0, 4, 8],
+      // [2, 4, 6],
     ];
     for (int i = 0; i < winningCombos.length; i++) {
       List<int> check = winningCombos[i];
